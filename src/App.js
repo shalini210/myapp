e6f183956a91d6c901b8edea38c378964ced32c4
shalini <s.rathore43@gmail.com>
2024-05-20 11:47:30 +0530

    fsd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import First from './First';
+import Revise from './Revise';
+import Headercomponent from './Headercomponent';
+import Menu from './Menu';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+        <Headercomponent></Headercomponent>
+        
+      <Revise></Revise>
+      <h1> my first reacgfcv t app </h1>
+      <First></First>
+      <h3>below we will use first component again </h3>
+      <First></First>
+      <Menu></Menu>
     </div>
   );
 }
